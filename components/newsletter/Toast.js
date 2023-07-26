acad0d7f3b8d6bb249f2c9d2e894cf55c2a71448
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-07-26 16:20:31 +0200

    Add toast render time

--- components/newsletter/Toast.js
+++ components/newsletter/Toast.js
@@ -11,22 +11,51 @@ export default class Toast extends HTMLComponent {
     this.subscription = JSON.parse(this.dataset.data);
     this.toast = this.root.querySelector('#toast');
     this.message = this.root.querySelector('p');
+    this.image = this.root.querySelector('.icon');
     this.close = this.root.querySelector('button');
+    this.bar = this.root.querySelector('.inner');
 
     this.render();
   }
 
   render() {
+    this.renderTime();
+
     // TODO: Validate
     if (this.subscription.email) {
-      this.toast.classList.add('success');
-      this.message.innerHTML = 'Success';
+      this.displaySuccess();
     }
 
     this.close.onClick(() => {
-      const main = document.querySelector('main');
-      main.removeChild(document.querySelector('toast-element'));
+      this.closeHandler();
     });
+  }
+
+  displaySuccess() {
+    this.toast.classList.add('success');
+    this.message.innerHTML = 'Success';
+    this.image.src = '/images/icons/done.png';
+  }
+
+  renderTime() {
+    let time = 100;
+    const distance = 40;
+    const close = this.closeHandler;
+
+    const intervalid = setInterval(() => {
+      time -= 1;
+      this.bar.style.width = `${time}%`;
+      this.bar.style.transition = `width ${distance}ms`;
+      if (time === 0) {
+        clearInterval(intervalid);
+        close();
+      }
+    }, distance);
+  }
+
+  closeHandler() {
+    const main = document.querySelector('main');
+    main.removeChild(document.querySelector('toast-element'));
   }
 }
 
